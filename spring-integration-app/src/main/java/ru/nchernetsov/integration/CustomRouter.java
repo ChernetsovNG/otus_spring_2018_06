b3cc3eeac8a9556278868b88937c3c27ef7fddf9
nchernetsov <n.chernetsov86@gmail.com>
2018-10-08 21:19:42 +0300

    HW15: custom router example

---
+++ spring-integration-app/src/main/java/ru/nchernetsov/integration/CustomRouter.java
@@ -0,0 +1,33 @@
+package ru.nchernetsov.integration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.router.AbstractMessageRouter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomRouter extends AbstractMessageRouter {
+
+    @Autowired
+    @Qualifier(value = "intChannel")
+    private MessageChannel intChannel;
+
+    @Autowired
+    @Qualifier(value = "stringChannel")
+    private MessageChannel stringChannel;
+
+    @Override
+    protected Collection<MessageChannel> determineTargetChannels(Message<?> message) {
+        Collection<MessageChannel> targetChannels = new ArrayList<>();
+        if (message.getPayload().getClass().equals(Integer.class)) {
+            targetChannels.add(intChannel);
+        } else if (message.getPayload().getClass().equals(String.class)) {
+            targetChannels.add(stringChannel);
+        }
+        return targetChannels;
+    }
+
+}
